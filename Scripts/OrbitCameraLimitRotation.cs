547653d4b86f0c40a53ba7ea51ff1d5693e1890d
RalivDev <ralivdev@gmail.com>
2025-04-10 22:13:49 -0500

    Fixed limit rotation inputs

--- Scripts/OrbitCameraLimitRotation.cs
+++ Scripts/OrbitCameraLimitRotation.cs
@@ -6,9 +6,25 @@ using UnityEngine;
 public class OrbitCameraLimitRotation : IOrbitCameraDataGenerator {
     [SerializeField, SerializeReference, SubclassSelector] protected IOrbitCameraDataGenerator input;
     [SerializeField] protected float angleFreedom = 30f;
-    [SerializeField] protected Transform targetRotationTransform;
+    [SerializeField] protected Quaternion targetRotation;
+    
+    public IOrbitCameraDataGenerator Input {
+        get => input;
+        set => input = value;
+    }
+
+    public float AngleFreedom {
+        get => angleFreedom;
+        set => angleFreedom = value;
+    }
+
+    public Quaternion TargetRotation {
+        get => targetRotation;
+        set => targetRotation = value;
+    }
+
     private Quaternion GetRotation(Quaternion camRotation) {
-        var rotation = targetRotationTransform.rotation;
+        var rotation = targetRotation;
         float angle = Quaternion.Angle(camRotation, rotation);
         if (angle > angleFreedom) {
             return Quaternion.RotateTowards(camRotation, rotation, angle - angleFreedom);
